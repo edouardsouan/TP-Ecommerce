bcdfe0284d01e65783e3106b7b68348441806bae
edouardsouan <souan.edouard0@gmail.com>
2014-04-12 18:13:58 +0200

    Modification AddCB
    Ajout des vérifiactions

---
+++ PLSQL.sql
@@ -0,0 +1,60 @@
+--------------------------------------------------------
+--  Fichier créé - samedi-avril-12-2014   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Procedure ADDUSER
+--------------------------------------------------------
+set define off;
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+--Ajout d'un client
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+  CREATE OR REPLACE PROCEDURE "ECOMMERCE"."ADDUSER" (p_CliNom CLIENT.CLI_NOM%TYPE,p_CliPrenom CLIENT.CLI_PRENOM%TYPE,p_Cli_MAIL CLIENT.CLI_MAIL%TYPE,p_CliPwd CLIENT.CLI_MOTDEPASSE%TYPE ) AS
+BEGIN
+INSERT INTO CLIENT(CLI_ID,CLI_NOM,CLI_PRENOM,CLI_MAIL,CLI_MOTDEPASSE)
+VALUES(SEQ_CLIENT.NEXTVAL,p_CliNom,p_CliPrenom,p_Cli_MAIL,p_CliPwd);
+END;
+/
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+--Ajout d'une CB à un client
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+create or replace PROCEDURE AddCB(p_IDUSER CLIENT.CLI_ID%TYPE, p_CBNUM CB.CB_NUMERO%TYPE, p_CBSECU CB.CB_SECU%TYPE, p_CBDATEEXP IN NVARCHAR2)
+AS
+    CURSOR cCBExiste IS SELECT CB_NUMERO FROM CB WHERE CB_NUMERO = p_CBNUM;
+    NumCB CB.CB_NUMERO%TYPE :=0;
+    CBAlreadySet EXCEPTION;
+    WrongCBNumber EXCEPTION;
+BEGIN
+
+--Vérification de l'unicité de la CB--
+    OPEN cCBExiste;
+    LOOP
+     FETCH cCBExiste INTO NumCB;
+     EXIT WHEN cCBExiste%NOTFOUND;
+    END LOOP;
+    CLOSE cCBExiste;
+ 
+    IF NumCB <> 0 THEN
+      RAISE CBAlreadySet;
+    ELSE
+        --Vérification de la longueur du numéro--
+      IF (length(p_CBNUM)=16) THEN
+          INSERT INTO CB(CB_ID,CLIENT_CLI_ID,CB_NUMERO,CB_SECU,CB_DATE)
+          VALUES(SEQ_CB.NEXTVAL,p_IDUSER,p_CBNUM,p_CBSECU,p_CBDATEEXP);
+      ELSE
+          RAISE WrongCBNumber;
+      END IF;
+  
+    END IF;
+ 
+  --Exceptions--
+  EXCEPTION
+    WHEN CBAlreadySet THEN
+      DBMS_OUTPUT.PUT_LINE('La carte existe déjà.');
+    WHEN WrongCBNumber THEN
+      DBMS_OUTPUT.PUT_LINE('Le numéro de la carte est incorrect. Veuillez vérifier la valeur saisie.');
+END;
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
